41f2bc0d9d8e3447b558cfab939573a0bdd2a1d4
김민수 <whitewolf23@naver.com>
2017-06-11 01:19:11 +0900

    no message

---
+++ Stanford_1_Model추가/Stanford_1/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  Stanford_1
+//
+//  Created by Myeongjin kyeong on 2017. 6. 7..
+//  Copyright © 2017년 Myeongjin kyeong. All rights reserved.
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var display: UILabel!
+    
+    //사용자가 입력중에 잇는 상태값 
+    //어딘 가의 저장
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender : UIButton){
+        
+        let digit = sender.currentTitle!
+        
+        if  userIsInTheMiddleOfTyping{
+            let textCurrentInDisplay = display!.text!
+            display!.text = textCurrentInDisplay + digit
+        }else{
+            display!.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+        
+        
+        print("\(digit) 클릭 됨 " )
+        
+    }
+    
+    //자동으로 디스플레이 안에 뭐가 있는 지 추적 
+    //연산 프로퍼티
+    var displayValue : Double{
+        //가져오기
+        //diplayValue값을 가져오기 위한 코드 
+        get{
+            return Double(display.text!)!
+        }
+        
+        //설정하기 
+        //누군가 변수를 설정할때 실행되는 코드
+        set{
+            display.text = String(newValue)
+        }
+    }
+    
+    
+    
+    @IBAction func performOperation(_ sender: UIButton){
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle{
+            switch mathematicalSymbol{
+                case "∏":
+//                display!.text = String(Double.pi)
+                displayValue = Double.pi
+
+                case "√":
+//                    let operand = Double(display!.text!)!
+//                    display!.text = String(sqrt(operand))
+                displayValue = sqrt(displayValue)
+ 
+            default:
+                break
+                
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+    }
+    
+    
+    
+}
+
